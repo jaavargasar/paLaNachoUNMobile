2f55c35b7779987b87fcaa5d502a371878bdca9a
jaavargasar <jaavargasar@gmail.com>
2017-10-07 00:52:32 -0500

    configuracion de ionic, cordoba y firebase

--- src/app/main.ts
+++ src/app/main.ts
@@ -2,4 +2,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
 
+import * as firebase from 'firebase/app'
+
+import {firebaseConfig} from '../environment/firebase-config'
+
+firebase.initializeApp(firebaseConfig);
+
+
 platformBrowserDynamic().bootstrapModule(AppModule);
